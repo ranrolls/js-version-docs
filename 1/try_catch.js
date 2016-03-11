56b37a7ac2db73746fc75457dd5e9c91e5f56ca4
Ranjan <greatwebist@outlook.com>
2016-03-11 10:19:55 +0530

    readme.md
    readme.md

---
+++ 1/try_catch.js
@@ -0,0 +1,15 @@
+try{
+    
+}catch(e){
+    if(e instanceof TypeError){
+        
+    }else if(e instanceof RangeError){
+        
+    }else if(e instanceof EvalError){
+        
+    }else{
+        
+    }
+}finally{
+    
+}
